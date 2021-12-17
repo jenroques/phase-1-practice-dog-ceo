9c21018ff493c074d9f3932c9d9fbae6fa9a0e89
Jennifer Roques <jenniferroques@gmail.com>
2021-12-17 16:16:16 -0600

    commit tree 7d975d49e984366f3fb5bc12181ef8065363edcf

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    let dogUL = document.querySelector("#dog-breeds")
+
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+        .then(response => response.json())
+        .then(handleImage)
+
+
+    fetch("https://dog.ceo/api/breeds/list/all")
+        .then(response => response.json())
+        .then(response => {
+            let dogBreedsArr = Object.keys(response.message)
+            dogBreedsArr.forEach((breed) => {
+                dogUL.innerHTML += `<li>${breed}</li>`
+            })
+        })
+    //DOMContentLoaded
+})
+
+function handleImage(jsonObject) {
+    let dogImages = document.getElementById('dog-image-container')
+    let dogArray = jsonObject.message;
+    dogArray.forEach(url => {
+        dogImages.innerHTML += imageTagString(url)
+    })
+}
+
+
+function imageTagString(url) {
+    return `<img src="${url}"/>`
+}
+
